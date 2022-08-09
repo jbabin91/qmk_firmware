3dd67a77dad67658e2430d12ef87ffc52e007749
Jace Babin <jbabin91@gmail.com>
2022-08-09 15:28:12 -0700

    Adding lastest keymap

--- keyboards/system76/launch_1/keymaps/jbabin91/keymap.c
+++ keyboards/system76/launch_1/keymaps/jbabin91/keymap.c
@@ -62,7 +62,7 @@ ________________________________________________________________________________
   [1] = LAYOUT(
     QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
-        KC_TILD, KC_EXLM, KC_AT,  KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_MINS, KC_EQL,  KC_TRNS,  KC_TRNS,
+        KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,    KC_5,   KC_6,   KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_TRNS,  KC_TRNS,
           KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_UNDS, KC_PLUS,  KC_LCBR, KC_RCBR, KC_PIPE,    KC_TRNS,     KC_TRNS,
             KC_TRNS, KC_F7,   KC_F8,   KC_9,    KC_F10, KC_F11,  KC_F12,  KC_TRNS, RGB_TOG,  RGB_VAD, RGB_VAI,   KC_TRNS,    KC_VOLU,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,       KC_MPLY,  KC_VOLD, KC_MNXT
@@ -94,7 +94,7 @@ ________________________________________________________________________________
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
         KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,    KC_5,   KC_6,   KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS, KC_EQL,  KC_TRNS,  KC_TRNS,
-          KC_TRNS, KC_F1,  KC_F2,  KC_F3,  KC_F4,   KC_F5,  KC_F6,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,    KC_TRNS,      KC_TRNS,
+          KC_TRNS, KC_F1,  KC_F2,  KC_F3,  KC_F4,   KC_F5,  KC_F6,  KC_MINS,  KC_EQL,  KC_LBRC,  KC_RBRC,  KC_BSLS,    KC_TRNS,      KC_TRNS,
             KC_TRNS, KC_F7,  KC_F8,  KC_9,   KC_F10,  KC_F11, KC_F12, KC_TRNS,  RGB_TOG,  RGB_VAD,  RGB_VAI,     KC_TRNS,    KC_VOLU,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,       KC_MPLY,  KC_VOLD, KC_MNXT
   ),
